9799f0897a0954df04eb2d2bc79ac2328a455520
jing guo <jingguo@jings-MacBook-Air.local>
2017-07-02 08:54:53 -0400

    jul2

--- ML/features/generate_decomposition_features.py
+++ ML/features/generate_decomposition_features.py
@@ -12,7 +12,7 @@ module_path = os.path.abspath(os.path.join('..'))
 sys.path.append(module_path)
 
 import pandas as pd
-from sklearn.decomposition import PCA, IncrementalPCA
+from sklearn.decomposition import PCA, IncrementalPCA, KernelPCA
 # remove warnings
 import warnings
 
@@ -33,18 +33,27 @@ def main():
     ids = conbined_data['ID']
     conbined_data.drop(['ID'], axis=1, inplace=True)
 
+    random_state = 100
+    n_jobs = -1
+
     # PCA
     n_comp = 50
-    pca = PCA(n_components=n_comp, random_state=100)
+    pca = PCA(n_components=n_comp, random_state=random_state)
     pca_df = pca.fit_transform(conbined_data)
     for i in range(0, n_comp):
         conbined_data['pca_' + str(i)] = pca_df[:, i]
 
     # IncrementalPCA
-    n_comp = 60
+    n_comp = 70
     ipca_df = IncrementalPCA(n_components=n_comp, batch_size=conbined_data.shape[0]).fit_transform(conbined_data)
     for i in range(0, n_comp):
         conbined_data['ipca_' + str(i)] = ipca_df[:, i]
+
+    # KernelPCA
+    n_comp = 60
+    ipca_df = KernelPCA(n_components=n_comp, kernel='linear', random_state=random_state, n_jobs=n_jobs).fit_transform(conbined_data)
+    for i in range(0, n_comp):
+        conbined_data['kernel_pca_' + str(i)] = ipca_df[:, i]
 
     conbined_data['ID'] = ids
     train = conbined_data.iloc[:train.shape[0], :]
