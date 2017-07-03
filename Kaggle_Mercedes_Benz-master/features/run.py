72af0279c7145acbd72e2a34f48be0d99b0204d8
jing guo <jingguo@jings-MacBook-Air.local>
2017-07-03 07:29:41 -0400

    Reset

---
+++ Kaggle_Mercedes_Benz-master/features/run.py
@@ -0,0 +1,29 @@
+#!/usr/local/miniconda2/bin/python
+# _*_ coding: utf-8 _*_
+
+"""
+@author: MarkLiu
+@time  : 17-6-29 下午2:21
+"""
+from __future__ import absolute_import, division, print_function
+
+import os
+import sys
+
+module_path = os.path.abspath(os.path.join('..'))
+sys.path.append(module_path)
+
+cmd = 'rm ../input/*_dataset.pkl'
+os.system(cmd)
+
+# cmd = 'python train_test_preprocess.py'
+# os.system(cmd)
+
+cmd = 'python perform_category_features.py'
+os.system(cmd)
+
+cmd = 'python generate_decomposition_features.py'
+os.system(cmd)
+
+cmd = 'python generate_groupby_features.py'
+os.system(cmd)
