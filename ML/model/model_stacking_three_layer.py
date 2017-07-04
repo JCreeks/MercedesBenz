95bdaa778aec7eafe1178e935add38eab17edbec
jing guo <jingguo@dyn-160-39-237-197.dyn.columbia.edu>
2017-07-04 19:08:15 -0400

    jul4

--- ML/model/model_stacking_three_layer.py
+++ ML/model/model_stacking_three_layer.py
@@ -20,6 +20,12 @@ warnings.filterwarnings('ignore')
 
 from sklearn.preprocessing import Imputer
 
+from sklearn.base import BaseEstimator,TransformerMixin, ClassifierMixin
+from sklearn.utils import check_array
+from sklearn.pipeline import make_pipeline
+from tpot.builtins import ZeroCount
+
+from xgboost import XGBRegressor
 from sklearn import preprocessing
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import ExtraTreesRegressor, GradientBoostingRegressor
@@ -27,17 +33,15 @@ from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge, Lasso
 from sklearn.linear_model import LassoCV,LassoLarsCV, ElasticNet
 from sklearn.neighbors import KNeighborsRegressor
-from sklearn.metrics import make_scorer, mean_squared_error
+from sklearn.metrics import make_scorer, mean_squared_error, r2_score
 
 from model_stack.model_wrapper import XgbWrapper, SklearnWrapper, GridCVWrapper
 from model_stack.model_stack import TwoLevelModelStacking, ThreeLevelModelStacking
 
 # my own module
-from features import data_utils
+from utils import data_util
 from conf.configure import Configure
-from readData import data_preprocess
-
-from magicNums import magicNums
+from CV import DeepCV, PrintImportance, StackingEstimator
 
 def RMSLE_(y_val, y_val_pred):
     return np.sqrt(np.mean((np.log(y_val+1)-np.log(y_val_pred+1))**2))
@@ -47,57 +51,19 @@ def RMSE_(y_val, y_val_pred):
     return np.sqrt(np.mean((y_val-y_val_pred)**2))
 RMSE = make_scorer(RMSE_, greater_is_better=False)
 
-train, test, macro = data_utils.load_data()
-#train.fillna(0, inplace=True)
-#test.fillna(0)
+R2 = make_scorer(r2_score, greater_is_better=True)
 
-isLog1p = False #True
-if (not isLog1p):
-    mult = .969
-    train['price_doc'] = train["price_doc"] * mult + 10
-    
-#####Magic Numbers!!!!!!!!!!!########
-#train = magicNums(train)
-#####################################
-    
-# train['price_doc'] = np.log1p(train['price_doc'])
-y_train = train['price_doc']
-id_train = train['id']
-train.drop(['id', 'price_doc'], axis=1, inplace=True)
-submit_ids = test['id']
-test.drop(['id'], axis=1, inplace=True)
+print 'load datas...'
+train, test = data_util.load_dataset()
 
-# 合并训练集和测试集
-conbined_data = pd.concat([train[test.columns.values], test])
-conbined_data.drop(['timestamp'], axis=1, inplace=True)
-print "conbined_data:", conbined_data.shape
+y_train = train['y']
+y_mean = np.mean(y_train)
+id_train = train['ID']
+del train['ID']
+del train['y']
+id_test = test['ID']
+del test['ID']
 
-# Deal with categorical values
-for c in conbined_data.columns:
-    if conbined_data[c].dtype == 'object':
-        lbl = preprocessing.LabelEncoder()
-        lbl.fit(list(conbined_data[c].values))
-        conbined_data[c] = lbl.transform(list(conbined_data[c].values))
-
-del conbined_data['school_education_centers_raion_ratio_dis']
-del conbined_data['preschool_education_centers_raion_ratio_dis']
-del conbined_data['sport_objects_raion_ratio_dis']
-del conbined_data['additional_education_raion_ratio_dis']
-del conbined_data['0_6_all_vs_preschool_quota_dis']
-
-conbined_data.replace([np.infty, -np.infty], np.nan)
-imp = Imputer(missing_values=np.nan, strategy='median')
-conbined_data=pd.DataFrame(imp.fit_transform(conbined_data), columns=conbined_data.columns)
-imp = Imputer(missing_values=np.infty, strategy='median')
-conbined_data=pd.DataFrame(imp.fit_transform(conbined_data), columns=conbined_data.columns)
-
-scaler = StandardScaler()
-conbined_data = scaler.fit_transform(conbined_data)
-
-train = conbined_data[:train.shape[0], :]
-test = conbined_data[train.shape[0]:, :]
-
-#train, test = data_preprocess(train, test)
 #############################
 #nTest = 500
 #train = train[:nTest]
@@ -108,36 +74,44 @@ test = conbined_data[train.shape[0]:, :]
 test_size = (1.0 * test.shape[0]) / train.shape[0]
 print "submit test size:", test_size
 
-rf_params1 = {'max_depth':6, 'n_jobs':-1, 'n_estimators':500, 'max_features':.95}
+train = np.array(train)
+test = np.array(test)
+
+rf_params1 = {'max_depth':16, 'n_estimators':250, 'min_samples_leaf':20, 'min_samples_split':60,
+              'max_features':.4, 'random_state':5, 'n_jobs':-1}
 
 rf_params2 = {'max_depth':12, 'min_samples_leaf':2, 'n_jobs':-1, 'n_estimators':100, 'max_features':.2}
 
-et_params1 = {'min_samples_leaf':2, 'max_depth':12, 'n_jobs':-1, 'n_estimators':100, 'max_features':.5}
+et_params1 = {'max_depth':13, 'n_estimators':200, 'min_samples_leaf':10,
+              'min_samples_split':20, 'max_features':.7, 'random_state':10, 'n_jobs':-1}
 
 et_params2 = {'min_samples_leaf':2, 'max_depth':12, 'n_jobs':-1, 'n_estimators':100, 'max_features':.5}
 
-gb_params1 = {'learning_rate':0.02, 'n_estimators':500, 'min_samples_leaf':70, 'min_samples_split':200, 
-              'max_features':'sqrt', 'max_depth':6, 'subsample':0.85}
+gb_params1 = {'learning_rate':0.1, 'n_estimators':50, 'min_samples_leaf':60,                                           'min_samples_split':20, 'subsample':0.8, 'max_features':.4,    
+              'max_depth':5}
 
-gb_params2 = {'n_estimators':500, 'max_features':15, 'max_depth':6, 'learning_rate':0.05, 'subsample':0.8}
+gb_params2 = {'learning_rate':0.001, 'loss':"huber", 'max_depth':3, 'max_features':0.55, 
+              'min_samples_leaf':18, 'min_samples_split':14, 'subsample':0.7}
 
-xgb_params1 = {'learning_rate':.05, 'subsample':.95, 'max_depth':4, 'min_child_weight':4, 'n_estimators':620, 
-              'colsample_bytree':0.95, 'gamma':.4, 'silent': 1, 'objective': 'reg:linear', 'eval_metric': 'rmse'}
+xgb_params1 = {'n_trees': 520, 'eta': 0.0045, 'max_depth': 4, 'subsample': 0.93, 
+               'objective': 'reg:linear', 'eval_metric': 'rmse', 'silent': 1, 'base_score': y_mean}
 
-xgb_params2 = {'learning_rate':.05, 'subsample':.7, 'max_depth':5, 'n_estimators':309, 'colsample_bytree':0.7, 
-               'silent': 1, 'objective': 'reg:linear', 'eval_metric': 'rmse'}
+xgb_params2 = {'eta': 0.005, 'max_depth': 4, 'subsample': 0.95, 'objective': 'reg:linear',
+               'eval_metric': 'rmse', 'silent': 1, 'base_score': y_mean}
 
-xgb_params3 = {'learning_rate':.1, 'subsample':.85, 'max_depth':4, 'min_child_weight':2, 'n_estimators':450, 
-              'colsample_bytree':0.85, 'gamma':.4, 'silent': 1, 'objective': 'reg:linear', 'eval_metric': 'rmse'}
+xgb_params3 = {'n_trees': 520, 'learning_rate': 0.0045, 'max_depth': 4, 'subsample': 0.94,  
+               'objective': 'reg:linear', 'eval_metric': 'rmse', 'base_score': y_mean, 
+               # base prediction = mean(target)             
+               'silent': 1, 'seed':5}
 
 xgb_params4 = {'learning_rate':.05, 'subsample':.6, 'max_depth':6, 'n_estimators':422, 'colsample_bytree':1, 
                'silent': 1, 'objective': 'reg:linear', 'eval_metric': 'rmse'}
 
-lcv_params = {'alphas' : [.1, 1, 10, 100, 1000]}# [1, 0.1, 0.001, 0.0005]}
+lcv_params = {'alphas' : [1, 0.1, 0.001, 0.0005]}#[.1, 1, 10, 100, 1000]}#
 
-rd_params = {'alpha': .1}
+rd_params = {'alpha': .5}
 
-ls_params = {'alpha':  100}#.0001}
+ls_params = {'alpha':  .01}#.0001}
 
 eln_params = {}
 
@@ -160,6 +134,14 @@ level_1_models = level_1_models + [SklearnWrapper(clf=KNeighborsRegressor,  para
                  SklearnWrapper(clf=KNeighborsRegressor,  params=knr_params2),
                  SklearnWrapper(clf=KNeighborsRegressor,  params=knr_params3),
                  SklearnWrapper(clf=KNeighborsRegressor,  params=knr_params4)]
+
+level_1_models = level_1_models + [make_pipeline( ZeroCount(), LassoLarsCV(normalize=True)),
+                 make_pipeline(StackingEstimator(estimator=LassoLarsCV(normalize=True)),
+                 StackingEstimator(estimator=GradientBoostingRegressor(learning_rate=0.001,
+                 loss="huber", max_depth=3, max_features=0.55, min_samples_leaf=18,
+                 min_samples_split=14, subsample=0.7)),
+                 LassoLarsCV())
+                                  ]
 
 params_list = [rf_params1, rf_params2, et_params1, et_params2, gb_params1, gb_params2, rd_params, ls_params, eln_params, lcv_params
                ]
@@ -214,7 +196,7 @@ ls = SklearnWrapper(clf=Lasso, seed=SEED, params=ls_params)
 #level_2_models = [SklearnWrapper(clf=ExtraTreesRegressor,seed=SEED,params={}),
 #                 XgbWrapper(seed=SEED, params=xgb_params1)]
 level_2_models = [xg, et, rf, rd, ls,
-                XgbWrapper(seed=SEED, params=xgb_params3)
+                #XgbWrapper(seed=SEED, params=xgb_params3)
                  ]
     
 # xgb_params = {
@@ -227,23 +209,18 @@ level_2_models = [xg, et, rf, rd, ls,
 #     'silent': 1
 # }
 
-if (isLog1p):
-    scoring = RMSE
-else:
-    scoring = RMSLE
-
 stacking_model = XgbWrapper(seed=SEED, params=xgb_params)
 #stacking_model = GridCVWrapper(Ridge, seed=SEED, cv_fold=5, params={}, scoring=scoring, param_grid = {
 #            'alpha': [1e-3,5e-3,1e-2,5e-2,1e-1,0.2,0.3,0.4,0.5,0.8,1e0,3,5,7,1e1]})
 
-#model_stack = TwoLevelModelStacking(train, y_train, test, level_2_models, stacking_model=stacking_model, stacking_with_pre_features=False, n_folds=5, random_seed=0, isLog1p=False)
+#model_stack = TwoLevelModelStacking(train, y_train, test, level_2_models, stacking_model=stacking_model, stacking_with_pre_features=False, n_folds=5, random_seed=0, )
 
 model_stack = ThreeLevelModelStacking(train, y_train, test, level_1_models, level_2_models, 
-stacking_model=stacking_model, stacking_with_pre_features=True, n_folds=5, random_seed=0, isLog1p=isLog1p)
+stacking_model=stacking_model, stacking_with_pre_features=False, n_folds=5, random_seed=0)
 
 predicts, score= model_stack.run_stack_predict()
 
-df_sub = pd.DataFrame({'id': submit_ids, 'price_doc': predicts})
+df_sub = pd.DataFrame({'ID': id_test, 'y': predicts})
 df_sub.to_csv(Configure.submission_path+str(score)+'.csv', index=False)
 
 
