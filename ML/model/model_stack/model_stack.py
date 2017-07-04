26db8ebb5adc3fd86c0e91a34729b6b7c134c3ae
JCreeks <gj19861103@gmail.com>
2017-07-04 18:00:18 -0400

    Update model_stack.py

--- ML/model/model_stack/model_stack.py
+++ ML/model/model_stack/model_stack.py
@@ -14,21 +14,17 @@ sys.path.append(module_path)
 import numpy as np
 import pandas as pd
 from math import sqrt
-from sklearn.metrics import mean_squared_error
+from sklearn.metrics import mean_squared_error, r2_score
 from sklearn.model_selection import KFold
 
 class TwoLevelModelStacking(object):
     """两层的 model stacking"""
 
     def __init__(self, train, y_train, test,
-                 models, stacking_model, isLog1p=True,
+                 models, stacking_model, 
                  stacking_with_pre_features=True, n_folds=5, random_seed=0):
         self.train = train
-        self.isLog1p=isLog1p
-        if (self.isLog1p):
-            self.y_train = np.log1p(y_train)
-        else:
-            self.y_train = y_train
+        self.y_train = y_train
         self.test = test
         self.n_folds = n_folds
         self.models = models
@@ -71,10 +67,7 @@ class TwoLevelModelStacking(object):
         # run level-1 out-of-folds
         for model in self.models:
             oof_train, oof_test = self.run_out_of_folds(model)
-            if (not self.isLog1p):
-                print("{}-1stCV: {}".format(model, sqrt(mean_squared_error(np.log1p(self.y_train), np.log1p(oof_train.clip(0,))))))
-            else:
-                print("{}-1stCV: {}".format(model, sqrt(mean_squared_error(self.y_train, oof_train))))
+            print("{}-1stCV: {}".format(model, r2_score(self.y_train, oof_train)))
             try:
                 x_train = np.concatenate((x_train, oof_train), axis=1)
                 x_test = np.concatenate((x_test, oof_test), axis=1)
@@ -89,12 +82,9 @@ class TwoLevelModelStacking(object):
             
         self.stacking_model.train(x_train, self.y_train)
        
-
         # stacking predict
         predicts = self.stacking_model.predict(x_test)
         score = self.stacking_model.getScore()
-        if (self.isLog1p):
-            predicts = np.expm1(predicts)
         print("stackingCV: {}".format(score))
         return predicts, score
     
@@ -102,17 +92,13 @@ class ThreeLevelModelStacking(object):
     """three layer model stacking"""
 
     def __init__(self, train, y_train, test,
-                 models, secondLevelModels, stacking_model, isLog1p=True,
+                 firstLevelModels, secondLevelModels, stacking_model, 
                  stacking_with_pre_features=True, n_folds=5, random_seed=0):
         self.train = train
-        self.isLog1p=isLog1p
-        if (self.isLog1p):
-            self.y_train = np.log1p(y_train)
-        else:
-            self.y_train = y_train
+        self.y_train = y_train
         self.test = test
         self.n_folds = n_folds
-        self.models = models
+        self.models = firstLevelModels
         self.secondLevelModels = secondLevelModels
         self.stacking_model = stacking_model
 
@@ -154,10 +140,7 @@ class ThreeLevelModelStacking(object):
         for model in self.models:
             oof_train, oof_test = self.run_out_of_folds(model)
             #print(oof_train)
-            if (not self.isLog1p):
-                print("{}-1stCV: {}".format(model, sqrt(mean_squared_error(np.log1p(self.y_train), np.log1p(oof_train.clip(0,))))))
-            else:
-                print("{}-1stCV: {}".format(model, sqrt(mean_squared_error(self.y_train, oof_train))))
+            print("{}-1stCV: {}".format(model, r2_score(self.y_train, oof_train)))
             try:
                 x_train = np.concatenate((x_train, oof_train), axis=1)
                 x_test = np.concatenate((x_test, oof_test), axis=1)
@@ -177,10 +160,7 @@ class ThreeLevelModelStacking(object):
             
         for model in self.secondLevelModels:
             oof_train, oof_test = self.run_out_of_folds(model)
-            if (not self.isLog1p):
-                print("{}-2ndCV: {}".format(model, sqrt(mean_squared_error(np.log1p(self.y_train), np.log1p(oof_train.clip(0,))))))
-            else:
-                print("{}-2ndCV: {}".format(model, sqrt(mean_squared_error(self.y_train, oof_train))))
+            print("{}-2ndCV: {}".format(model, r2_score(self.y_train, oof_train)))
             try:
                 x_train = np.concatenate((x_train, oof_train), axis=1)
                 x_test = np.concatenate((x_test, oof_test), axis=1)
@@ -202,10 +182,8 @@ class ThreeLevelModelStacking(object):
         predicts = self.stacking_model.predict(x_test)
         score = self.stacking_model.getScore()
         
-        pd.DataFrame(train_to_save).to_csv("1stLayerX_trainIsLog1p_{}_{}.csv".format(self.isLog1p, score))
-        pd.DataFrame(test_to_save).to_csv("1stLayerX_testIsLog1p_{}_{}.csv".format(self.isLog1p, score))
+        #pd.DataFrame(train_to_save).to_csv("1stLayerX_train_{}.csv".format(score))
+        #pd.DataFrame(test_to_save).to_csv("1stLayerX_test_{}.csv".format(score))
         
-        if (self.isLog1p):
-            predicts = np.expm1(predicts)
         print("stackingCV: {}".format(score))
         return predicts, score
