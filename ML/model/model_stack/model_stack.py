43a3211a6c464462c642da3e5b346fd6c9c109fb
jing guo <jingguo@jings-MacBook-Air.local>
2017-07-08 09:41:26 -0400

    jul8

--- ML/model/model_stack/model_stack.py
+++ ML/model/model_stack/model_stack.py
@@ -87,6 +87,7 @@ class TwoLevelModelStacking(object):
         score = self.stacking_model.getScore()
         print("stackingCV: {}".format(score))
         return predicts, score
+
     
 class ThreeLevelModelStacking(object):
     """three layer model stacking"""
@@ -135,6 +136,194 @@ class ThreeLevelModelStacking(object):
         #else:
         #    x_train = np.empty((self.ntrain, self.train.shape[1]))
         #    x_test = np.empty((self.ntest, self.test.shape[1]))
+
+        # run level-1 out-of-folds
+        for model in self.models:
+            oof_train, oof_test = self.run_out_of_folds(model)
+            #print(oof_train)
+            print("{}-1stCV: {}".format(model, r2_score(self.y_train, oof_train)))
+            try:
+                x_train = np.concatenate((x_train, oof_train), axis=1)
+                x_test = np.concatenate((x_test, oof_test), axis=1)
+            except:
+                x_train = oof_train
+                x_test = oof_test
+        
+        train_to_save = x_train
+        test_to_save = x_test
+        
+        # run level-2 out-of-folds
+        self.train = x_train
+        self.test = x_test
+        
+        #x_train = np.empty((self.ntrain, self.train.shape[1]))
+        #x_test = np.empty((self.ntest, self.test.shape[1]))
+            
+        for model in self.secondLevelModels:
+            oof_train, oof_test = self.run_out_of_folds(model)
+            print("{}-2ndCV: {}".format(model, r2_score(self.y_train, oof_train)))
+            try:
+                x_train = np.concatenate((x_train, oof_train), axis=1)
+                x_test = np.concatenate((x_test, oof_test), axis=1)
+            except:
+                x_train = oof_train
+                x_test = oof_test
+            
+        # run level-3 stacking
+        #xgbWrapper only
+        best_nrounds, cv_mean, cv_std = self.stacking_model.cv_train(x_train, self.y_train)
+        self.stacking_model.nrounds = best_nrounds
+        print('Ensemble-CV: {0}+{1}'.format(cv_mean, cv_std))
+        #xgbWrapper only
+        
+        self.stacking_model.train(x_train, self.y_train)
+       
+
+        # stacking predict
+        predicts = self.stacking_model.predict(x_test)
+        score = self.stacking_model.getScore()
+        
+        #pd.DataFrame(train_to_save).to_csv("1stLayerX_train_{}.csv".format(score))
+        #pd.DataFrame(test_to_save).to_csv("1stLayerX_test_{}.csv".format(score))
+        
+        print("stackingCV: {}".format(score))
+        return predicts, score
+
+class TwoLevelModelStacking_new(object):
+    """两层的 model stacking"""
+
+    def __init__(self, train, y_train, test,
+                 models, stacking_model, 
+                 stacking_with_pre_features=True, n_folds=5, random_seed=0, newFeat_train=pd.DataFrame(),
+                 newFeat_test=pd.DataFrame(), stacking_with_new_features=True):
+        self.train = train
+        self.y_train = y_train
+        self.test = test
+        self.n_folds = n_folds
+        self.models = models
+        self.stacking_model = stacking_model
+        self.newFeat_train = newFeat_train
+        self.newFeat_test = newFeat_test
+
+        # stacking_with_pre_features 指定第二层 stacking 是否使用原始的特征
+        self.stacking_with_pre_features = stacking_with_pre_features
+        self.stacking_with_new_features = stacking_with_new_features
+
+        self.ntrain = train.shape[0]
+        self.ntest = test.shape[0]
+        self.kfold = KFold(n_splits=n_folds, shuffle=True, random_state=random_seed)
+
+    def run_out_of_folds(self, clf):
+        oof_train = np.zeros((self.ntrain,))
+        oof_test = np.zeros((self.ntest,))
+        oof_test_skf = np.empty((self.n_folds, self.ntest))
+
+        for i, (train_index, test_index) in enumerate(self.kfold.split(self.train)):
+            #print 'fold-{}: train: {}, test: {}'.format(i, train_index, test_index)
+            x_tr = self.train[train_index]
+            y_tr = self.y_train[train_index]
+            x_te = self.train[test_index]
+
+            clf.train(x_tr, y_tr)
+
+            oof_train[test_index] = clf.predict(x_te)
+            oof_test_skf[i, :] = clf.predict(self.test)
+
+        oof_test[:] = oof_test_skf.mean(axis=0)
+        return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+    def run_stack_predict(self):
+        if self.stacking_with_pre_features:
+            x_train = self.train
+            x_test = self.test
+        if self.stacking_with_new_features:
+            try:
+                x_train = np.concatenate((x_train, self.newFeat_train), axis=1)
+                x_test = np.concatenate((x_test, self.newFeat_test), axis=1)
+            except:
+                x_train = self.newFeat_train
+                x_test = self.newFeat_test
+
+        # run level-1 out-of-folds
+        for model in self.models:
+            oof_train, oof_test = self.run_out_of_folds(model)
+            print("{}-1stCV: {}".format(model, r2_score(self.y_train, oof_train)))
+            try:
+                x_train = np.concatenate((x_train, oof_train), axis=1)
+                x_test = np.concatenate((x_test, oof_test), axis=1)
+            except:
+                x_train = oof_train
+                x_test = oof_test
+
+        # # run level-2 stacking
+        best_nrounds, cv_mean, cv_std = self.stacking_model.cv_train(x_train, self.y_train)
+        self.stacking_model.nrounds = best_nrounds
+        print('Ensemble-CV: {0}+{1}'.format(cv_mean, cv_std))
+            
+        self.stacking_model.train(x_train, self.y_train)
+       
+        # stacking predict
+        predicts = self.stacking_model.predict(x_test)
+        score = self.stacking_model.getScore()
+        print("stackingCV: {}".format(score))
+        return predicts, score
+
+class ThreeLevelModelStacking_new(object):
+    """three layer model stacking"""
+
+    def __init__(self, train, y_train, test,
+                 firstLevelModels, secondLevelModels, stacking_model, 
+                 stacking_with_pre_features=True, n_folds=5, random_seed=0, newFeat_train=pd.DataFrame(),
+                 newFeat_test=pd.DataFrame(), stacking_with_new_features=True):
+        self.train = train
+        self.y_train = y_train
+        self.test = test
+        self.n_folds = n_folds
+        self.models = firstLevelModels
+        self.secondLevelModels = secondLevelModels
+        self.stacking_model = stacking_model
+        self.newFeat_train = newFeat_train
+        self.newFeat_test = newFeat_test
+
+        # stacking_with_pre_features 指定第二层 stacking 是否使用原始的特征
+        self.stacking_with_pre_features = stacking_with_pre_features
+        self.stacking_with_new_features = stacking_with_new_features
+
+        self.ntrain = train.shape[0]
+        self.ntest = test.shape[0]
+        self.kfold = KFold(n_splits=n_folds, shuffle=True, random_state=random_seed)
+
+    def run_out_of_folds(self, clf):
+        oof_train = np.zeros((self.ntrain,))
+        oof_test = np.zeros((self.ntest,))
+        oof_test_skf = np.empty((self.n_folds, self.ntest))
+
+        for i, (train_index, test_index) in enumerate(self.kfold.split(self.train)):
+            #print 'fold-{}: train: {}, test: {}'.format(i, train_index, test_index)
+            x_tr = self.train[train_index]
+            y_tr = self.y_train[train_index]
+            x_te = self.train[test_index]
+
+            clf.train(x_tr, y_tr)
+
+            oof_train[test_index] = clf.predict(x_te)
+            oof_test_skf[i, :] = clf.predict(self.test)
+
+        oof_test[:] = oof_test_skf.mean(axis=0)
+        return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
+    def run_stack_predict(self):
+        if self.stacking_with_pre_features:
+            x_train = self.train
+            x_test = self.test
+       
+        if self.stacking_with_new_features:
+            try:
+                x_train = np.concatenate((x_train, self.newFeat_train), axis=1)
+                x_test = np.concatenate((x_test, self.newFeat_test), axis=1)
+            except:
+                x_train = self.newFeat_train
+                x_test = self.newFeat_test
 
         # run level-1 out-of-folds
         for model in self.models:
--- stacked.py
+++ stacked.py
@@ -18,8 +18,10 @@ from sklearn.random_projection import SparseRandomProjection
 from sklearn.decomposition import PCA, FastICA
 from sklearn.decomposition import TruncatedSVD
 from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import KFold
 
-
+seed=0
 
 class StackingEstimator(BaseEstimator, TransformerMixin):
     
@@ -42,8 +44,8 @@ class StackingEstimator(BaseEstimator, TransformerMixin):
         return X_transformed
 
 
-train = pd.read_csv('../input/train.csv')
-test = pd.read_csv('../input/test.csv')
+train = pd.read_csv('ML/input/train.csv')
+test = pd.read_csv('ML/input/test.csv')
 
 for c in train.columns:
     if train[c].dtype == 'object':
@@ -83,7 +85,7 @@ srp_results_test = srp.transform(test)
 
 #save columns list before adding the decomposition components
 
-usable_columns = list(set(train.columns) - set(['y']))
+#usable_columns = list(set(train.columns) - set(['y']))
 
 # Append decomposition components to datasets
 for i in range(1, n_comp + 1):
@@ -102,7 +104,7 @@ for i in range(1, n_comp + 1):
     train['srp_' + str(i)] = srp_results_train[:, i - 1]
     test['srp_' + str(i)] = srp_results_test[:, i - 1]
 
-#usable_columns = list(set(train.columns) - set(['y']))
+usable_columns = list(set(train.columns) - set(['y']))
 
 y_train = train['y'].values
 y_mean = np.mean(y_train)
@@ -111,7 +113,8 @@ id_test = test['ID'].values
 finaltrainset = train[usable_columns].values
 finaltestset = test[usable_columns].values
 
-
+# train.to_csv('ML/input/newFeat_train.csv')
+# test.to_csv('ML/input/newFeat_test.csv')
 '''Train the xgb model then predict the test data'''
 
 xgb_params = {
@@ -122,7 +125,8 @@ xgb_params = {
     'objective': 'reg:linear',
     'eval_metric': 'rmse',
     'base_score': y_mean, # base prediction = mean(target)
-    'silent': 1
+    'silent': 1,
+    'seed': seed
 }
 # NOTE: Make sure that the class is labeled 'class' in the data file
 
@@ -131,14 +135,14 @@ dtest = xgb.DMatrix(test)
 
 num_boost_rounds = 1250
 # train model
-model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=num_boost_rounds)
+model = xgb.train(dict(xgb_params, silent=1), dtrain, num_boost_round=num_boost_rounds)
 y_pred = model.predict(dtest)
 
 '''Train the stacked models then predict the test data'''
 
 stacked_pipeline = make_pipeline(
     StackingEstimator(estimator=LassoLarsCV(normalize=True)),
-    StackingEstimator(estimator=GradientBoostingRegressor(learning_rate=0.001, loss="huber", max_depth=3, max_features=0.55, min_samples_leaf=18, min_samples_split=14, subsample=0.7)),
+    StackingEstimator(estimator=GradientBoostingRegressor(random_state=seed, learning_rate=0.001, loss="huber", max_depth=3, max_features=0.55, min_samples_leaf=18, min_samples_split=14, subsample=0.7)),
     LassoLarsCV()
 
 )
@@ -147,17 +151,68 @@ stacked_pipeline = make_pipeline(
 stacked_pipeline.fit(finaltrainset, y_train)
 results = stacked_pipeline.predict(finaltestset)
 
+# pd.DataFrame(stacked_pipeline.predict(finaltrainset), columns=['stackPip']).to_csv('ML/input/stacked_pipeline_train.csv', index=False)
+# pd.DataFrame(results, columns=['stackPip']).to_csv('ML/input/stacked_pipeline_test.csv', index=False)
+
+
+#####################################
+# oof_train = np.zeros((len(finaltrainset),))
+# oof_test = np.zeros((len(finaltestset),))
+# oof_test_skf = np.empty((5, len(finaltestset)))
+# kfold = KFold(n_splits=5, shuffle=True, random_state=0)
+# clf = stacked_pipeline
+
+# for i, (train_index, test_index) in enumerate(kfold.split(finaltrainset)):
+#     #print 'fold-{}: train: {}, test: {}'.format(i, train_index, test_index)
+#     x_tr = finaltrainset[train_index]
+#     y_tr = y_train[train_index]
+#     x_te = finaltrainset[test_index]
+
+#     clf.fit(x_tr, y_tr)
+
+#     oof_train[test_index] = clf.predict(x_te)
+#     oof_test_skf[i, :] = clf.predict(finaltestset)
+
+# oof_test[:] = oof_test_skf.mean(axis=0)
+
+# pd.DataFrame(oof_train.reshape(-1, 1), columns=['stackPip']).to_csv('ML/input/stacked_pipeline_train.csv', index=False)
+# pd.DataFrame(oof_test.reshape(-1, 1), columns=['stackPip']).to_csv('ML/input/stacked_pipeline_test.csv', index=False)
+#######################################
+
 '''R2 Score on the entire Train data when averaging'''
 
 print('R2 score on train data:')
-print(r2_score(y_train,stacked_pipeline.predict(finaltrainset)*0.2855 + model.predict(dtrain)*0.7145))
+#print(r2_score(y_train,stacked_pipeline.predict(finaltrainset)*0.2855 + model.predict(dtrain)*0.7145))
+
+n_folds = 10
+train_scores = []
+val_scores = []
+num_boost_roundses = []
+metric = r2_score
+df_columns = train.columns.values
+for i in range(0, n_folds):
+    random_state = 42 + i
+    X_fit, X_val, y_fit, y_val = train_test_split(train, y_train, test_size=0.25, random_state=random_state)
+
+    y_mean_fit = np.mean(y_fit)
+    dtfit = xgb.DMatrix(X_fit.drop('y', axis=1), y_fit)
+    dtval = xgb.DMatrix(X_val.drop('y', axis=1), y_fit)
+    train_score = metric(y_fit, stacked_pipeline.predict(X_fit[usable_columns].values)*0.2855 + model.predict(dtfit)*0.7145)
+    val_score = metric(y_val, stacked_pipeline.predict(X_val[usable_columns].values)*0.2855 + model.predict(dtval)*0.7145)
+    print 'perform {} cross-validate: train r2 score = {}, validate r2 score = {}'.format(i + 1, train_score, val_score)
+    train_scores.append(train_score)
+    val_scores.append(val_score)
+
+print '\naverage train r2 score = {}, average validate r2 score = {}'.format(
+    sum(train_scores) / len(train_scores),
+    sum(val_scores) / len(val_scores))
 
 '''Average the preditionon test data  of both models then save it on a csv file'''
 
 sub = pd.DataFrame()
 sub['ID'] = id_test
 sub['y'] = y_pred*0.75 + results*0.25
-sub.to_csv('stacked-models.csv', index=False)
+sub.to_csv('ML/result/stacked-models_moreFeats.csv', index=False)
 
 
 # Any results you write to the current directory are saved as output.
